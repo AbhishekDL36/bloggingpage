9349df5a552ed84eb8319ec5aef7b71419e12a3c
AbhishekDL36 <abhishekofficials12@gmail.com>
2025-02-06 16:24:06 +0530

    added a button to go back to blogs list

--- src/FirstBlog.jsx
+++ src/FirstBlog.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import data from "./res.json"
+import { Link } from 'react-router-dom'
 export default function FirstBlog() {
    const blog= useSelector(state=>state.blogclicked)
   
     const author = data[blog].author
+    const content = data[blog].info
   return (
-    <div>
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center",gap:"20px",flexDirection:"column",height:"84vh",background:"#A39DCD"}}>
    <p>  BLOG NO YOU OPENED IS : {blog+1}</p> 
 <p> by -: {author} </p> 
-
+<p>{content}</p>
+<Link to="/"><button style={{background:"transparent", padding:"20px"}}>back to list of blogs</button> </Link> 
     </div>
   )
 }
